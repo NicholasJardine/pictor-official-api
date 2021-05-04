3e6e8d2d8e31f9bffe798c518277da5aa695f63b
Nicholas Jardine <nicholasjardine0@gmail.com>
2021-05-04 16:44:28 +0200

    new forms in

---
+++ db/migrate/20210504141007_removeindexfromusers.rb
@@ -0,0 +1,4 @@
+class Removeindexfromusers < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_03_115916) do
+ActiveRecord::Schema.define(version: 2021_05_04_141007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
