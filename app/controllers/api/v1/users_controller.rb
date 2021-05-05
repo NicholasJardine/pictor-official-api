5b9726e641f5e477b35f44c677692de0e4e0eedd
Nicholas Jardine <nicholasjardine0@gmail.com>
2021-05-05 12:28:00 +0200

    controller and views changed

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,18 +1,41 @@
-class Api::V1::UsersController < ActionController::Base
- def index
- @users = User.all
- render json: @users
- end
- def show
- @user = User.find(params[:id])
- render json: @user
- end
- def create
- @user = User.create(user_params)
- render json: @user
- end
- private
- def user_params
- params.require(:user).permit(:email, :password, :password_confirmation)
- end
+  class Api::V1::UsersController < Api::V1::BaseController
+
+  def index
+    @users = policy_scope(User)
+  end
+
+
+  def show
+    @user = User.find(params[:id])
+    authorize @user
+  end
+
+  private
+
+  def render_error
+    render json: { errors: @user.errors.full_messages },
+      status: :unprocessable_entity
+  end
+
 end
+
+# class Api::V1::UsersController < ActionController::Base
+#  def index
+#  @users = User.all
+#  render json: @users
+#  end
+#  def show
+#  @user = User.find(params[:id])
+#  render json: @user
+#  end
+#  def create
+#  @user = User.create(user_params)
+#  render json: @user
+#  end
+#  private
+#  def user_params
+#  params.require(:user).permit(:email, :password, :password_confirmation)
+#  end
+# end
+
+
