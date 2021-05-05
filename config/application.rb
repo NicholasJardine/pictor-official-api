ea10e1d38904bb10a31353e4a47963bd386ef9da
Nicholas Jardine <nicholasjardine0@gmail.com>
2021-05-05 10:43:48 +0200

    cors removed and  extra lines

--- config/application.rb
+++ config/application.rb
@@ -15,21 +15,6 @@ module PictorOfficialApi
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.api_only = true
-    config.middleware.use ActionDispatch::Flash
-
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource(
-          '*',
-          headers: :any,
-          methods: [:get, :patch, :put, :delete, :post, :options]
-          )
-      end
-    end
-
-
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
